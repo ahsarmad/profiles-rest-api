39a564c058bbc3f207962f420b61a8d07f45d73d
Sarmad <afzalsarmad77@gmail.com>
2022-08-13 16:36:48 -0400

    literally just changeme

--- projectfiles_project/settings.py
+++ projectfiles_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@5urjl2u#yi4!me1!2qpo8-cjh%wmli1id##z^zg=-7+bp79-b'
+SECRET_KEY = 'changeme'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
