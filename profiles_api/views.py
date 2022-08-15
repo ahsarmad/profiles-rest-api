18e125ef059575af5be64cb6c60eff44c96458ca
Sarmad <afzalsarmad77@gmail.com>
2022-08-15 18:00:00 -0400

    added filtering to user profile api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,11 +1,11 @@
 from curses import A_VERTICAL
 from email import message
-from re import T
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from profiles_api import serializers
 from profiles_api import models
@@ -106,3 +106,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication, )
     permission_classes = (permissions.UpdateOwnProfile, )
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('name', 'email', )
