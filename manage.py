f4b17148d4b1b86d16053d53875a4ede04096c20
Sarmad <afzalsarmad77@gmail.com>
2022-08-18 00:01:45 -0400

    cleanup

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                          'profiles_project.settings')
+                          'projectfiles_project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
